05b88433f7e5bd682fad86b0852d1d31a1b52d83
mohadmin25 <abuhamar008@gmail.com>
2022-08-12 23:12:08 +0300

    lenovo

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.querySelector("main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";    
